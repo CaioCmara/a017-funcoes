7a5bbac08c6b9e0c853752339d59358f8044d2f7
CaioCmara <caio1801@gmail.com>
2022-09-14 18:41:20 -0300

    Exercicio resolvido

---
+++ exercicio3/script.js
@@ -0,0 +1,24 @@
+let primeiroNum = prompt("Insira o primeiro número")
+let segundoNum = prompt("Insira o segundo número")
+
+function soma (num1, num2){
+    console.log(num1 + num2)
+}
+soma(primeiroNum, segundoNum)
+
+function sub (num1, num2){
+    console.log(num1 - num2)
+}
+sub(primeiroNum, segundoNum)
+
+function multi(num1, num2){
+    console.log(num1 * num2)
+}
+multi(primeiroNum, segundoNum)
+
+function div(num1, num2){
+    console.log(num1 / num2)
+}
+div(primeiroNum, segundoNum)
+
+ 
